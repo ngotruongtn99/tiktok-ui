e6aeee25f7a390b1603dd2e986662049cbb69122
ngotruongtn99 <ngotruongtn99@gmail.com>
2022-06-17 10:13:59 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import "./App.css";
-import Button from "./components/Button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "~/components/Layout";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <Button />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Upload() {
+  return (
+    <div>
+      <h2>Upload Page</h2>
+    </div>
+  );
+}
